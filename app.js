2898807e8ef36388984cc290093e099014ce30f2
atsu <yamahata.atsushi.as01@gmail.com>
2023-11-14 10:13:14 +0900

    update app

--- app.js
+++ app.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const session = require('express-session');
-const mysql = require('mysql');
+const mysql = require('mysql2/promise');
 const app = express();
 const port = 5000;
 
 // 初期設定
-require(__dirname + '/settings/bootstrap.js')(app, session);
+require(__dirname + '/settings/bootstrap.js')(express, app, session);
+
+const mysql_connect = require(__dirname + '/settings/db_connecting.js')(mysql);
 
 // ルーティング
-require(__dirname + '/settings/route.js')(app);
+require(__dirname + '/settings/route.js')(app, mysql_connect);
 
 // サーバー起動
-app.listen(port, ()=>{console.log('run')});
+app.listen(port, ()=>{
+    console.log('success')
+});

d861b5d7343f6c96ef5ea21b1fd98e3e3ff132d6
atsuPueri <yamahata.atsushi.as01@gmail.com>
2023-11-14 17:14:13 +0900

    add css folder

--- settings/bootstrap.js
+++ settings/bootstrap.js
@@ -2,15 +2,18 @@ module.exports = function(express, app, session) {
     
     // ビューエンジンをEJSに設定
     app.set('view engine', 'ejs');
-    app.set('views', __dirname +'/../views')
-
+    app.set('views', __dirname +'/../views');
+    
     // セッション
     app.use(session({
         secret: 'secret', // よくわかってない
         resave: false,
         saveUninitialized: false
     }));
-
+    
     // フォームから受け取れる設定
     app.use(express.urlencoded({extended:true}));
+
+    // 静的ファイル設定
+    app.use(express.static(__dirname + '/../public'));
 };

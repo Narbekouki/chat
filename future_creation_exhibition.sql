859e78a22c105398d4ecfa6077f5b4d281ef72d6
atsu <yamahata.atsushi.as01@gmail.com>
2023-11-14 10:15:30 +0900

    update package and sql

--- future_creation_exhibition.sql
+++ future_creation_exhibition.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.0.2
+-- version 5.2.0
 -- https://www.phpmyadmin.net/
 --
 -- ホスト: 127.0.0.1
--- 生成日時: 2023-11-10 07:15:57
+-- 生成日時: 2023-11-14 01:59:25
 -- サーバのバージョン： 10.4.24-MariaDB
--- PHP のバージョン: 7.4.6
+-- PHP のバージョン: 8.1.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,36 +24,110 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- テーブルの構造 `joined_room`
+--
+
+CREATE TABLE `joined_room` (
+  `room_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `joined_room`
+--
+
+INSERT INTO `joined_room` (`room_id`, `user_id`) VALUES
+(1, 96),
+(1, 97);
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `message`
+--
+
+CREATE TABLE `message` (
+  `id` int(11) NOT NULL,
+  `room_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL COMMENT '送信者ID',
+  `text` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- テーブルの構造 `room`
+--
+
+CREATE TABLE `room` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `room`
+--
+
+INSERT INTO `room` (`id`, `name`, `create_at`) VALUES
+(1, 'test', '2023-11-14 00:15:32');
+
+-- --------------------------------------------------------
+
+--
 -- テーブルの構造 `user`
 --
 
 CREATE TABLE `user` (
   `id` int(11) NOT NULL,
   `login_id` text NOT NULL,
-  `passward_hash` text NOT NULL,
+  `password_hash` text NOT NULL,
   `name` text NOT NULL,
-  `icon_path` text NOT NULL
+  `icon_path` text NOT NULL,
+  `create_at` datetime NOT NULL DEFAULT current_timestamp(),
+  `update_at` datetime NOT NULL DEFAULT current_timestamp() ON UPDATE current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+--
+-- テーブルのデータのダンプ `user`
+--
+
+INSERT INTO `user` (`id`, `login_id`, `password_hash`, `name`, `icon_path`, `create_at`, `update_at`) VALUES
+(96, 'atsu', '$2b$10$Cj9OG6yRkNl0ShR.BpGCE.6bWBZAKqQ4NnlV9NZXlRUtNBKF2.alC', 'atsu', '', '2023-11-12 17:56:33', '2023-11-12 17:56:33'),
+(97, 'yamada', 'pass', '山田', '', '2023-11-14 00:16:16', '2023-11-14 00:16:16');
 
 --
 -- ダンプしたテーブルのインデックス
 --
 
 --
--- テーブルのインデックス `user`
+-- テーブルのインデックス `room`
 --
-ALTER TABLE `user`
+ALTER TABLE `room`
   ADD PRIMARY KEY (`id`);
 
 --
--- ダンプしたテーブルのAUTO_INCREMENT
+-- テーブルのインデックス `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `login_id` (`login_id`) USING HASH;
+
+--
+-- ダンプしたテーブルの AUTO_INCREMENT
 --
 
 --
--- テーブルのAUTO_INCREMENT `user`
+-- テーブルの AUTO_INCREMENT `room`
+--
+ALTER TABLE `room`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- テーブルの AUTO_INCREMENT `user`
 --
 ALTER TABLE `user`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=98;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
